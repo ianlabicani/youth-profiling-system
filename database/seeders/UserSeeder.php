1d1dadb95edca62a19c9649a9c543454694a0324
ianlabicani <labicaniian@gmail.com>
2025-11-09 10:53:49 +0800

    initial commit

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Barangay SK Official
+        $barangayUser = User::create([
+            'name' => 'Barangay SK Official',
+            'email' => 'barangay@mail.com',
+            'password' => Hash::make('password'),
+        ]);
+        $barangayUser->roles()->attach(Role::where('name', 'BARANGAY')->first());
+
+        // Municipal SK Official
+        $municipalUser = User::create([
+            'name' => 'Municipal SK Official',
+            'email' => 'municipal@mail.com',
+            'password' => Hash::make('password'),
+        ]);
+        $municipalUser->roles()->attach(Role::where('name', 'MUNICIPAL')->first());
+
+        // Provincial SK Official
+        $provincialUser = User::create([
+            'name' => 'Provincial SK Official',
+            'email' => 'provincial@mail.com',
+            'password' => Hash::make('password'),
+        ]);
+        $provincialUser->roles()->attach(Role::where('name', 'PROVINCIAL')->first());
+    }
+}
